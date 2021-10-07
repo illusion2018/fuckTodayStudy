da57dc73f1fbbd99d5ba02fb90ba8fdd98bad743
illusion2018 <45729285+illusion2018@users.noreply.github.com>
2021-10-07 18:54:20 +0800

    Update casLogin.py

--- login/casLogin.py
+++ login/casLogin.py
@@ -98,7 +98,8 @@ class casLogin:
             print(data,self.type)
             if self.type == 0:
                 print('485489465156')
-                #msg = soup.select('#errorMsg')[0].get_text()
+                msg = soup.select('#errorMsg')[0].get_text()
+                msg='smwy'
                 print(msg)
             else:
                 msg = soup.select('#formErrorTip2')[0].get_text()
